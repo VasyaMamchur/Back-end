3c0a8f59183a4949968ed32a9bf0d90da9d0b734
Vasya Mamchur <vasyamamchur089@gmail.com>
2023-12-17 20:26:30 +0200

    Add config.py

---
+++ mymodule/config.py
@@ -0,0 +1,12 @@
+PROPAGATE_EXCEPTIONS = True
+FLASK_DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Kirito124@localhost:5432/Back-end"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+API_TITLE = "Finance REST API"
+API_VERSION = "v1"
+OPENAPI_VERSION = "3.0.3"
+OPENAPI_URL_PREFIX = "/"
+OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
