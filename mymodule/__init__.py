3b2b97da0003b27c149ad837e1161f62a19d3599
Vasya Mamchur <vasyamamchur089@gmail.com>
2023-12-17 20:27:14 +0200

    Update __init__.py and views.py

--- mymodule/__init__.py
+++ mymodule/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py', silent=True)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import mymodule.views
+import mymodule.models
--- mymodule/views.py
+++ mymodule/views.py
@@ -1,116 +1,187 @@
-from flask import jsonify, request
-
-from mymodule import app
-from mymodule.models import User, Category, Record
-from datetime import datetime
-
-users = {}
-categories = {}
-records = {}
-record_id_counter = 1
-
-
-@app.route("/user/<int:user_id>", methods=['GET', 'DELETE'])
-def get_delete_user(user_id):
-    user = users.get(user_id)
-    if not user:
-        return jsonify(message='User not found'), 404
-
-    if request.method == 'GET':
-        return jsonify(id=user.user_id, name=user.name)
-    elif request.method == 'DELETE':
-        del users[user_id]
-        return jsonify(message='User deleted successfully')
+from mymodule import app, db
+from flask import request, jsonify
+from marshmallow import *
+from . import schemas
+from .models import UserModel
+from .models import BankAccountModel
+from .models import CategoryModel
+from .models import RecordModel
+from . import models
 
 
-@app.route("/users", methods=['GET'])
-def get_users():
-    return jsonify(users=[{'id': user.user_id, 'name': user.name} for user in users.values()])
+def add(data):
+    db.session.add(data)
+    db.session.commit()
 
 
-@app.route("/user", methods=['POST'])
+def update():
+    db.session.commit()
+
+
+def delete(data):
+    db.session.delete(data)
+    db.session.commit()
+
+
+@app.post('/user')
 def create_user():
-    data = request.get_json()
-    if 'name' not in data:
-        return jsonify(message='Name is required'), 400
-
-    user_id = len(users) + 1
-    new_user = User(user_id, data['name'])
-    users[user_id] = new_user
-
-    return jsonify(message='User created successfully', id=user_id)
-
-
-@app.route("/category/<int:category_id>", methods=['GET', 'DELETE'])
-def get_delete_category(category_id):
-    category = categories.get(category_id)
-    if not category:
-        return jsonify(message='Category not found'), 404
-
-    if request.method == 'GET':
-        return jsonify(id=category.category_id, name=category.name)
-    elif request.method == 'DELETE':
-        del categories[category_id]
-        return jsonify(message='Category deleted successfully')
+    user_data = request.get_json()
+    try:
+        schemas.UserSchema().load({"name": user_data["name"]})
+        if db.session.query(UserModel).filter(UserModel.name == user_data["name"]).first() is None:
+            user = UserModel(user_data["name"])
+            add(user)
+            schemas.BankAccountSchema().load({"user_id": user.id, "money": user_data["money"]})
+            account = BankAccountModel(user.id, user_data["money"])
+            add(account)
+            return jsonify({"id": user.id, "name": user.name, "money": account.money})
+        else:
+            return "UserExist, 404"
+    except ValidationError as error:
+        return error.messages
 
 
-@app.route("/categories", methods=['GET'])
-def get_categories():
-    return jsonify(categories=[{'id': category.category_id, 'name': category.name} for category in categories.values()])
+@app.get('/user/account')
+def get_account():
+    user_id = request.args.get('userID')
+    try:
+        account = db.session.query(BankAccountModel).filter(BankAccountModel.user_id == user_id).first()
+        return jsonify({"user_id": account.user_id, "money": account.money})
+    except AttributeError:
+        return "AttributeError, 404"
 
 
-@app.route("/category", methods=['POST'])
+@app.get('/user')
+def get_user():
+    user_id = request.args.get('userID')
+    try:
+        user = db.session.query(UserModel).filter(UserModel.id == user_id).first()
+        return jsonify({"id": user.id, "name": user.name})
+    except AttributeError:
+        return "AttributeError, 404"
+
+
+@app.delete('/user')
+def delete_user():
+    user_id = request.args.get('userID')
+    try:
+        user = db.session.query(UserModel).filter(UserModel.id == user_id).first()
+        deleted_user = {"id": user.id, "name": user.name}
+        delete(user)
+        return jsonify(deleted_user)
+    except AttributeError:
+        return "AttributeError, 404"
+
+
+@app.get('/users')
+def get_users():
+    users = [{"id": user.id, "name": user.name} for user in db.session.query(UserModel).all()]
+    return jsonify(users)
+
+
+@app.post('/category')
 def create_category():
-    data = request.get_json()
-    if 'name' not in data:
-        return jsonify(message='Name is required'), 400
-
-    category_id = len(categories) + 1
-    new_category = Category(category_id, data['name'])
-    categories[category_id] = new_category
-
-    return jsonify(message='Category created successfully', id=category_id)
-
-
-@app.route("/record/<int:record_id>", methods=['GET', 'DELETE'])
-def get_delete_record(record_id):
-    record = records.get(record_id)
-    if not record:
-        return jsonify(message='Record not found'), 404
-
-    if request.method == 'GET':
-        return jsonify(id=record.record_id, user_id=record.user_id, category_id=record.category_id,
-                       created_at=record.created_at, amount=record.amount)
-    elif request.method == 'DELETE':
-        del records[record_id]
-        return jsonify(message='Record deleted successfully')
+    category_data = request.get_json()
+    try:
+        schemas.CategorySchema().load({"name": category_data["name"]})
+        if db.session.query(CategoryModel).filter(CategoryModel.name == category_data["name"]).first() is None:
+            category = CategoryModel(category_data["name"])
+            add(category)
+            return jsonify({"id": category.id, "name": category.name})
+        else:
+            return "CategoryExist, 404"
+    except ValidationError as error:
+        return error.messages
 
 
-@app.route("/records", methods=['GET'])
-def get_records():
-    user_id = request.args.get("user_id")
-    category_id = request.args.get("category_id")
-    if not user_id and not category_id:
-        return {"message": "Required parameters are not specified"}, 400
-    filter = records
-    if user_id:
-        filter = {record_id: record for record_id, record in filter.items() if record.user_id == user_id}
-    if category_id:
-        filter = {record_id: record for record_id, record in filter.items() if record.category_id == category_id}
-    return jsonify(records=[{'id': record.record_id, 'user_id': record.user_id, 'category_id': record.category_id, 'created_at': record.created_at, 'amount': record.amount} for record in filter.values()])
+@app.get('/category')
+def get_category():
+    category_id = request.args.get('categoryID')
+    try:
+        schemas.CategorySchema().load({"id": category_id, "name": "instance"})
+        try:
+            category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+            return jsonify({"id": category.id, "name": category.name})
+        except AttributeError:
+            return "AttributeError, 404"
+    except ValidationError as error:
+        return error.messages
 
 
-@app.route("/record", methods=['POST'])
+@app.get('/categories')
+def get_categories():
+    categories = [{"id": category.id, "name": category.name} for category in db.session.query(CategoryModel).all()]
+    return jsonify(categories)
+
+
+@app.delete('/category')
+def delete_category():
+    category_id = request.args.get('categoryID')
+    try:
+        category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+        deleted_category = {"id": category.id, "name": category.name}
+        delete(category)
+        return jsonify(deleted_category)
+    except AttributeError:
+        return "AttributeError, 404"
+
+
+@app.post('/record')
 def create_record():
-    data = request.get_json()
-    required_fields = ['user_id', 'category_id', 'amount']
-    for field in required_fields:
-        if field not in data:
-            return jsonify(message=f'{field} is required'), 400
+    user_id = request.args.get('userID')
+    category_id = request.args.get('categoryID')
+    amount_of_expenditure = request.args.get('amount')
+    try:
+        schemas.RecordSchema().load(
+            {"user_id": user_id, "category_id": category_id, "amount_of_expenditure": amount_of_expenditure})
+        if db.session.get(models.UserModel, user_id) is not None and db.session.get(models.CategoryModel,
+                                                                                    category_id) is not None:
+            account = db.session.query(models.BankAccountModel).filter(models.BankAccountModel.user_id == user_id).first()
+            if account is not None and account.money - float(amount_of_expenditure) > 0:
+                account.money = account.money - float(amount_of_expenditure)
+                update()
+                record = models.RecordModel(user_id, category_id, amount_of_expenditure)
+                add(record)
+                return jsonify(
+                    {"id": record.id, "user_id": record.user_id, "category_id": record.category_id, "time": record.time,
+                     "amount_of_expenditure": record.amount_of_expenditure})
+            else:
+                return "user account isn't exist or insufficient funds"
+        else:
+            return "user or category isn't exist"
+    except ValidationError as error:
+        return error.messages
 
-    global record_id_counter
-    new_record = Record(record_id_counter, data['user_id'], data['category_id'], datetime.now(), data['amount'])
-    records[record_id_counter] = new_record
-    record_id_counter += 1
 
-    return jsonify(message='Record created successfully', id=record_id_counter - 1)
+@app.get('/record')
+def get_record():
+    user_id = request.args.get('userID')
+    category_id = request.args.get('categoryID')
+    try:
+        schemas.RecordSchema().load({"user_id": user_id, "category_id": category_id, "amount_of_expenditure": 1.0})
+        try:
+            record = db.session.query(RecordModel).filter(RecordModel.user_id == user_id,
+                                                          RecordModel.category_id == category_id).first()
+            return jsonify(
+                {"id": record.id, "user_id": record.user_id, "category_id": record.category_id, "time": record.time,
+                 "amount_of_expenditure": record.amount_of_expenditure})
+        except AttributeError:
+            return "AttributeError, 404"
+    except ValidationError as error:
+        return error.messages
+
+
+@app.delete('/record')
+def delete_record():
+    record_id = request.args.get('recordID')
+    if record_id.isdigit():
+        try:
+            record = db.session.query(RecordModel).filter(RecordModel.id == record_id).first()
+            deleted_record = {"id": record.id, "user_id": record.user_id, "category_id": record.category_id,
+                              "time": record.time, "amount_of_expenditure": record.amount_of_expenditure}
+            delete(record)
+            return jsonify(deleted_record)
+        except AttributeError:
+            return "AttributeError, 404"
+    else:
+        return "sqlalchemy.exc.DataError"
