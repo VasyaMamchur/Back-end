88b254f635d5fba60dad3fc1ae8b87def2733b30
Vasya Mamchur <vasyamamchur089@gmail.com>
2023-12-21 20:16:49 +0200

    Update __init__.py and views.py

--- mymodule/__init__.py
+++ mymodule/__init__.py
@@ -1,58 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import psycopg2
-
-def get_db_connection():
-    return psycopg2.connect(host="dpg-cm041ned3nmc738j45bg-a", database="back_end_1vxx", user="admin", password="zROLWMZmaXBWInlu4wHHV4sh11MgH3kF")
-
-
-
-def create_tables():
-    commands = (
-        """ 
-        CREATE TABLE users (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR (20) NOT NULL);
-        """,
-        """ 
-        CREATE TABLE accounts (
-                id SERIAL PRIMARY KEY,
-                user_id INT,
-                money INT NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users (id)
-                );
-        """,
-        """ 
-        CREATE TABLE categories ( 
-                id SERIAL PRIMARY KEY, 
-                name VARCHAR(20) NOT NULL 
-                );
-        """,
-        """ 
-        CREATE TABLE records ( 
-                id SERIAL PRIMARY KEY,
-                user_id INT,
-                category_id INT, 
-                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                amount_of_expenditure FLOAT NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users (id),
-                FOREIGN KEY (category_id) REFERENCES categories (id)
-                );
-        """)
-    conn = None
-    try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        for command in commands:
-            cur.execute(command)
-        cur.close()
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+from .db_func import get_db_connection, create_tables
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
